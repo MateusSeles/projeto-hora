54770e2eb111da6b4f475ca6082246467ce16eed
Mateus Seles <114114748+MateusSeles@users.noreply.github.com>
2023-02-19 15:38:38 -0300

    Att

---
+++ script.js
@@ -0,0 +1,33 @@
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var h1 = window.document.getElementById('h1')
+    var data = new Date()
+    var hora = data.getHours()
+    var min = data.getMinutes()
+    var dia = data.getDate()
+    var mes = (data.getMonth() + 1)
+    var ano = data.getUTCFullYear()
+    //var hora =  
+    msg.innerHTML = `<strong>Agora são ${hora}:${min}<br>${dia}/${mes}/${ano}`
+    document.body.style.background = 'blue'
+    if (hora >= 0 && hora < 12) {
+        // bom dia
+        msg.innerHTML += `<br>Tenha um ótimo dia!! Good Morning!!`
+        img.src = 'imagens/dia.png.jpg'
+        h1.innerText = 'Oie'
+        document.body.style.background = 'yellow'
+    } else if (hora >= 12 && hora <= 18) {
+        //boa tarde
+        msg.innerHTML += `<br>Tenha uma ótima tarde! Good Evening!!`
+        img.src = 'imagens/tarde.png.jpg'
+        document.body.style.background = 'Darkorange'
+        document.body.header.h1 = 'Tenha uma ótima noite/ Good Night!!'
+    } else {
+        // boa noite
+        msg.innerHTML += `<br>Boa noite flor da noite`
+        img.src = 'imagens/noite.png.jpg'
+        document.body.style.background = 'black'
+    }
+
+}
